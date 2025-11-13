860b30ceb80553895c159c4c57ba5a50bdc23696
unknown <jrakotonirina91@gmail.com>
2025-11-13 15:49:05 +0100

    réglage de la CI 2

--- src/app.js
+++ src/app.js
@@ -17,9 +17,7 @@ const options = {
 };
 
 const swaggerSpec = swaggerJSDoc(options);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(z));
-
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const storage = require('./services/storage');
 
